b962da4e04d13a942e92e1f10d00966d666dc4ee
Mira Kikani <kikanimira288@gmail.com>
2023-10-22 12:49:52 +0530

    create cart and orders

--- demoapp/models.py
+++ demoapp/models.py
@@ -1,5 +1,7 @@
 from django.db import models
-
+from django.db import models
+from django.contrib.auth.models import BaseUserManager, AbstractBaseUser
+from django.contrib.auth.models import User
 
 #  Database manipulation with Meta Class
 # https://docs.djangoproject.com/en/4.2/ref/models/options/
@@ -56,3 +58,106 @@ class Products(models.Model):
     
     class Meta:
         verbose_name_plural = "Products"
+
+
+class UserProfile(models.Model):
+    contact_detail = models.BigIntegerField()
+    date_of_birth = models.DateField()
+    gender = models.CharField(max_length=20)
+    profile_image = models.ImageField()
+    user_model = models.ForeignKey(User, on_delete=models.CASCADE)
+
+class Cart(models.Model):
+    product = models.ForeignKey(Products, on_delete=models.CASCADE)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    quantity = models.IntegerField()
+
+PAYMENT_TERMS = [
+    ("COD", "COD"),
+    ("Online", "Online"),
+    ("EMI", "EMI"),
+]
+
+ORDER_STATUS = [
+    ("Order Placed", "Order Placed"),
+    ("Shipped", "Shipped"),
+    ("Out for delivery", "Out for delivery"),
+    ("Delivered", "Delivered"),
+    ("Returned", "Returned"),
+]
+class Orders(models.Model):
+    order_number = models.CharField(max_length=100, blank=True, null=True)
+    product = models.ForeignKey(Products, on_delete=models.CASCADE)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    total_amount = models.IntegerField()
+    address = models.ForeignKey(Address, on_delete=models.CASCADE)
+    payment_terms = models.CharField(max_length = 20, choices = PAYMENT_TERMS)
+    order_status = models.CharField(max_length = 20, choices = ORDER_STATUS, default="Order Placed")
+
+
+# class MyUserManager(BaseUserManager):
+#     def create_user(self, email, date_of_birth, password=None):
+#         """
+#         Creates and saves a User with the given email, date of
+#         birth and password.
+#         """
+#         if not email:
+#             raise ValueError("Users must have an email address")
+
+#         user = self.model(
+#             email=self.normalize_email(email),
+#             date_of_birth=date_of_birth,
+#         )
+
+#         user.set_password(password)
+#         user.save(using=self._db)
+#         return user
+
+#     def create_superuser(self, email, date_of_birth, password=None):
+#         """
+#         Creates and saves a superuser with the given email, date of
+#         birth and password.
+#         """
+#         user = self.create_user(
+#             email,
+#             password=password,
+#             date_of_birth=date_of_birth,
+#         )
+#         user.is_admin = True
+#         user.save(using=self._db)
+#         return user
+
+
+# class MyUser(AbstractBaseUser):
+#     email = models.EmailField(
+#         verbose_name="email address",
+#         max_length=255,
+#         unique=True,
+#     )
+#     date_of_birth = models.DateField()
+#     is_active = models.BooleanField(default=True)
+#     is_admin = models.BooleanField(default=False)
+
+#     objects = MyUserManager()
+
+#     USERNAME_FIELD = "email"
+#     REQUIRED_FIELDS = ["date_of_birth"]
+
+#     def __str__(self):
+#         return self.email
+
+#     def has_perm(self, perm, obj=None):
+#         "Does the user have a specific permission?"
+#         # Simplest possible answer: Yes, always
+#         return True
+
+#     def has_module_perms(self, app_label):
+#         "Does the user have permissions to view the app `app_label`?"
+#         # Simplest possible answer: Yes, always
+#         return True
+
+#     @property
+#     def is_staff(self):
+#         "Is the user a member of staff?"
+#         # Simplest possible answer: All admins are staff
+#         return self.is_admin
--- demoapp/urls.py
+++ demoapp/urls.py
@@ -10,4 +10,6 @@ urlpatterns = [
     path('user_registration/', views.user_registration, name='user_registration'),
     path('user_login/', views.user_login, name='user_login'),
     path('user_logout/', views.user_logout, name='user_logout'),
+    path('add_to_cart/', views.add_to_cart, name='add_to_cart'),
+    path('order_placed/', views.order_placed, name='order_placed'),
 ]
